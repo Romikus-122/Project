e0dc9e28e32a6184010c91bb64835cc2f74d122e
kaononai <sapkinbogdan1@gmail.com>
2025-01-21 23:18:16 +0300

    доска

--- tetris.py
+++ tetris.py
@@ -4,6 +4,26 @@ import sys
 import pygame
 
 
+def draw_board(screen):
+    left = 800
+    top = 300
+    cell = 40
+    for i in range(15):
+        for j in range(8):
+            pygame.draw.rect(screen, (16, 16, 16),
+                             ((left + cell * j, top + cell * i), (cell, cell)), 1)
+            pygame.draw.rect(screen, "white", ((left - 3, top - 3), (cell * 8 + 6, cell * 15 + 6)), 2)
+            pygame.draw.rect(screen, "white", ((left - 15, top - 15), (cell * 8 + 30, cell * 15 + 30)), 5)
+
+
+def draw_text(screen):
+    left = 300
+    right = 1220
+    f1 = pygame.font.Font(None, 135)
+    header = f1.render('Тетрис', 0, "white")
+    screen.blit(header, (800, 175))
+
+
 def load_image(name):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
@@ -25,5 +45,6 @@ def tetris():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-#тестовый коммит !! еще один
+        draw_board(screen)
+        draw_text(screen)
         pygame.display.flip()
