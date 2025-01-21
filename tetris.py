bbf3dba470cbdf6ed293858718c6834bf86608d8
kaononai <sapkinbogdan1@gmail.com>
2025-01-21 22:34:57 +0300

    тест гита 6

--- tetris.py
+++ tetris.py
@@ -25,5 +25,5 @@ def tetris():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-#тестовый коммит !!!
+#тестовый коммит !! еще один
         pygame.display.flip()
