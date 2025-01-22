9bbc73e65b7c3be1a4d9e186867f24e1527a9fe7
kaononai <sapkinbogdan1@gmail.com>
2025-01-22 23:24:39 +0300

    тетрис без геймплея но с крутой анимацией

--- tetris.py
+++ tetris.py
@@ -4,24 +4,60 @@ import sys
 import pygame
 
 
+def draw_background(screen, grid_pos):
+    screen.fill((10, 0, 20))
+    for i in range(20):
+        # первая сетка (движется вправо)
+        pygame.draw.line(screen, (20, 0, 40), (-1080 + 300 * i + grid_pos, 1080), (0 + 300 * i + grid_pos, 0), 4)
+        pygame.draw.line(screen, (20, 0, 40), (3000 - 300 * i + grid_pos, 1080), (1920 - 300 * i + grid_pos, 0), 4)
+        pygame.draw.line(screen, (20, 0, 40), (-1080 + 150 * i + grid_pos + 40, 1080), (0 + 150 * i + grid_pos + 40, 0), 2)
+        pygame.draw.line(screen, (20, 0, 40), (3000 - 150 * i + grid_pos + 40, 1080), (1920 - 150 * i + grid_pos + 40, 0), 2)
+
+        # вторая сетка (движется влево)
+        pygame.draw.line(screen, (20, 0, 40), (-1080 + 300 * i - grid_pos, 1080), (0 + 300 * i - grid_pos, 0), 4)
+        pygame.draw.line(screen, (20, 0, 40), (3000 - 300 * i - grid_pos, 1080), (1920 - 300 * i - grid_pos, 0), 4)
+        pygame.draw.line(screen, (20, 0, 40), (-1080 + 150 * i - grid_pos - 40, 1080), (0 + 150 * i - grid_pos - 40, 0), 2)
+        pygame.draw.line(screen, (20, 0, 40), (3000 - 150 * i - grid_pos - 40, 1080), (1920 - 150 * i - grid_pos - 40, 0), 2)
+
+
 def draw_board(screen):
-    left = 800
-    top = 300
+    left = 750
+    top = 250
     cell = 40
-    for i in range(15):
-        for j in range(8):
-            pygame.draw.rect(screen, (16, 16, 16),
+    pygame.draw.rect(screen, (5, 0, 10), ((left, top), (cell * 10, cell * 18)))
+    for i in range(18):
+        for j in range(10):
+            pygame.draw.rect(screen, (10, 0, 20),
                              ((left + cell * j, top + cell * i), (cell, cell)), 1)
-            pygame.draw.rect(screen, "white", ((left - 3, top - 3), (cell * 8 + 6, cell * 15 + 6)), 2)
-            pygame.draw.rect(screen, "white", ((left - 15, top - 15), (cell * 8 + 30, cell * 15 + 30)), 5)
+            pygame.draw.rect(screen, "white", ((left - 3, top - 3), (cell * 10 + 6, cell * 18 + 6)), 2)
+            pygame.draw.rect(screen, "white", ((left - 15, top - 15), (cell * 10 + 30, cell * 18 + 30)), 5)
 
 
-def draw_text(screen):
-    left = 300
+def draw_text(screen, best, score):
+# инициализация текста
+    left = 800
     right = 1220
     f1 = pygame.font.Font(None, 135)
+    f2 = pygame.font.Font(None, 40)
+
     header = f1.render('Тетрис', 0, "white")
-    screen.blit(header, (800, 175))
+    best_score = f2.render(f'Лучший результат: {best}', 0, "white")
+    now_score = f2.render(f'Текущий результат: {score}', 0, "white")
+    next_shape = f2.render('Следующая фигура:', 0, "white")
+    pause_text1 = f2.render('Чтобы поставить на паузу,', 0, "white")
+    pause_text2 = f2.render('нажмите кнопку P', 0, "white")
+
+# перекрывающий фон холст для текста
+    pygame.draw.rect(screen, (5, 0, 10), ((1200, 250), (400, 500)))
+    pygame.draw.rect(screen, "white", ((1200, 250), (400, 500)), 2)
+
+# отрисовка текста
+    screen.blit(header, (left, 125))
+    screen.blit(best_score, (right, 270))
+    screen.blit(now_score, (right, 330))
+    screen.blit(next_shape, (right, 390))
+    screen.blit(pause_text1, (right, 650))
+    screen.blit(pause_text2, (right, 700))
 
 
 def load_image(name):
@@ -38,13 +74,23 @@ def tetris():
     pygame.display.set_caption('Тетрис')
     size = width, height = 1920, 1080
     screen = pygame.display.set_mode(size)
+    grid_pos = 0
     fps = 60
+
+    score = 0
+    best = 0
 
     running = True
     while running:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        grid_pos += 1
+        if grid_pos >= 300:
+            grid_pos = 0
+        draw_background(screen, grid_pos)
+
         draw_board(screen)
-        draw_text(screen)
+        draw_text(screen, best, score)
         pygame.display.flip()
